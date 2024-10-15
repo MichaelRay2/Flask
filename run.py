84930a7051c779385ca802496ce683b6c085d755
Michael Ray <michaelray2809@yahoo.com>
2024-10-15 15:50:11 +0000

    Completed contact form

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
@@ -18,9 +21,22 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return "<h1>" + member["name"] + "</h1>"
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -32,5 +48,5 @@ def careers():
 if __name__=="__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("Port", "5080")),
+        port=int(os.environ.get("Port", "6010")),
         debug=True)
